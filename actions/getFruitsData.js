56d2b23d32439482fe509dc62a74b0f59b2cb6da
Mohammed Dayyan <161062356+MDAYYAN-007@users.noreply.github.com>
2024-08-03 23:14:04 +0530

    Update getFruitsData.js

--- actions/getFruitsData.js
+++ actions/getFruitsData.js
@@ -7,14 +7,16 @@ const getFruitsData = async (email) => {
     const userResult = await sql`SELECT id FROM farmtotable_users WHERE email = ${email}`;
     const userId = userResult.rows[0].id;
 
+    // Fetch products where product_type is 'fruits', user_id is not the given userId, and available_units is not 0
     const response = await sql`
-      SELECT * FROM farmtotable_products WHERE product_type = 'fruits' AND user_id != ${userId};
+      SELECT * FROM farmtotable_products
+      WHERE product_type = 'fruits' 
+        AND user_id != ${userId}
+        AND available_units != 0;
     `;
-
     if (response && response.rows && response.rows.length > 0) {
       return response.rows;
     }
-    return [];
   } catch (error) {
     console.error('Error fetching fruits data:', error);
     return [];
